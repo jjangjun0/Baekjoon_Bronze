553ed5aac928905cc7cd50f855257f3aa9721efc
jjangjun0 <jangjun012@gmail.com>
2025-08-08 01:06:17 +0900

    updated file

---
+++ 5/10872_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, x, t;
+
+    cin >> n >> x;
+    while (n--) {
+        cin >> t;
+        if (t < x) cout << t << ' ';
+    }
+
+}
